802e1e5c23d4b7a4b1453de457352e5a2126b000
maty1975 <matiasgomezaranda@gmail.com>
2024-12-30 21:53:22 -0300

    cambios
    ahora tiene una particula cuando se mueven y detecta bien cuando salta y cuando no

---
+++ SCRIPTS/efectosalto.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var rbplayer: RigidBody2D = $".."
+@onready var particula_suelo: CPUParticles2D = $"../particula suelo"
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if rbplayer.linear_velocity.y <= 10:
+		particula_suelo.emitting = true
--- SCRIPTS/movimiento.gd
+++ SCRIPTS/movimiento.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 @onready var ray_cast_2d_suelo: RayCast2D = $"RayCast2D suelo"
+@onready var particle_left: CPUParticles2D = $particle_left
+@onready var particle_right: CPUParticles2D = $particle_right
 
 var movement : Vector2
 var gravedad_default : int
@@ -16,13 +18,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	print(saltando)
+	print(linear_velocity)
 	if Input.is_action_pressed("ui_left"):
 		set_axis_velocity(-movement)
-		
-	elif Input.is_action_pressed("ui_right"):
+		particle_right.emitting = true
+	else:
+		particle_right.emitting = false
+	
+	
+	if Input.is_action_pressed("ui_right"):
 		set_axis_velocity(movement)
-		
+		particle_left.emitting = true
+	else:
+		particle_left.emitting = false
 	salto()
 	rayo()
 
@@ -31,6 +39,7 @@ func salto():
 		if (saltando == false):
 			apply_impulse(Vector2(0,-2000))
 			gravity_scale = gravedad
+			
 	elif Input.is_action_just_released("Spacebar"):
 		if (saltando ==true):
 			gravity_scale = gravedad * 4
@@ -43,8 +52,15 @@ func rayo():
 	else:
 		desactivar_salto()
 		
+		
 func activar_salto():
 	saltando = false
+	#if linear_velocity.y >= 50:
+		#print("SHAKY")
 
 func desactivar_salto():
 	saltando = true
+	
+
+func fuerza_suelo():
+	pass
